d884de36d077984213fb6b5384e4374cdbb9766b
Inesh Dey <ineshdey@hotmail.com>
2024-12-05 02:18:56 -0800

    complete!!!

--- src/Home.jsx
+++ src/Home.jsx
@@ -270,6 +270,29 @@ function Page8() {  // eslint-disable-line
         </>
     )
 }
+function Page9() {  // eslint-disable-line
+    const {scene, cameras} = useGLTF('/static/models/page9.glb')
+    const {set} = useThree()
+    const terrainRef = useRef()
+    const lastFrameTime = useRef(Date.now())
+    useEffect(() => {
+        if (cameras.length > 0) {
+            cameras[0].fov = 30
+            cameras[0].updateProjectionMatrix()
+            set({camera: cameras[0]})
+        }
+    }, [cameras, set])
+    return (
+        <>
+            <primitive
+                object={scene}
+                castShadow
+                receiveShadow
+            />
+            <Environment files="/static/hdri/goegap_road_2k.hdr"/>
+        </>
+    )
+}
 export default function App() {
     const [counter, setCounter] = useState(0)
 
@@ -357,9 +380,9 @@ export default function App() {
                             className={'absolute p-[10%] w-[70%] left-[30%] h-full z-10 text-left text-[2.5vh] flex flex-col'}>
                             <p>
                                 Battle Ground is a crater lake, meaning it formed by a volcanic cone, which over time
-                                became filled with precipitation such as rain and snow (McDonald). It was formed around
-                                100,000 years ago (Genis), and is part of the Boring Lava Field, around the
-                                Portland-Vancouver border between Washington and Oregon (Ruth).
+                                became filled with precipitation such as rain and snow (McDonald, 2009). It was formed around
+                                100,000 years ago (Genis, 2021), and is part of the Boring Lava Field, around the
+                                Portland-Vancouver border between Washington and Oregon (Ruth, 2018).
                             </p>
                             <button onClick={incrementCounter}
                                     className={'m-[1vh] bg-blue-500 hover:bg-blue-700 text-white py-[0.5vh] px-4 rounded-full font-sans text-[2vh] ml-auto transition-colors'}>
@@ -374,7 +397,7 @@ export default function App() {
                                 position: 'absolute',
                                 top: 0,
                                 left: 0,
-                                zIndex: 20,
+                                zIndex: 0,
                                 pointerEvents: "none"
                             }}
                             shadows
@@ -408,7 +431,7 @@ export default function App() {
                             className={'absolute p-[10%] w-[70%] left-[30%] h-full z-10 text-left text-[2.5vh] flex flex-col'}>
                             <p><h2 className={'text-[4vh] font-bold font-sans'}>Summary</h2>
                                 The name "Battle Ground" seems pretty odd for a natural park. It's named after the city
-                                next to it, which never truly was a "battle ground" (Ruth). Rather, it was named after a
+                                next to it, which never truly was a "battle ground" (Ruth, 2018). Rather, it was named after a
                                 standoff between Captain William Strong and a group of Klickitat escapees...
                             </p>
                             <button onClick={incrementCounter}
@@ -458,14 +481,14 @@ export default function App() {
                         <div
                             className={'absolute p-[10%] w-[70%] left-[30%] h-full z-10 text-left text-[2.5vh] flex flex-col'}>
                             <h2 className={'text-[4vh] font-bold font-sans'}>The Yakima War</h2>
-                            <p className={"h-[50%] overflow-y-scroll"}>
+                            <p className={"h-[75%] overflow-y-scroll"}>
                                 The lake’s name of Battle Ground is taken from the nearby town of Battle Ground, who
                                 themselves got their name by interesting means. Its name goes all the way back to 1855,
                                 during the Yakima War, a conflict between Americans and the Yakima plus their allies
-                                (Caldbick). The conflict led to imprisonment of many American Indians to avoid the
-                                chance of their revolt (Caldbick), akin to the method in which Japanese would later be
+                                (Caldbick, 2010). The conflict led to imprisonment of many American Indians to avoid the
+                                chance of their revolt (Caldbick, 2010), akin to the method in which Japanese would later be
                                 detained in World War II. One of these groups were the Klickitat, captured by the
-                                Vancouver Barracks, an early military post in the Pacific Northwest (Caldbick).
+                                Vancouver Barracks, an early military post in the Pacific Northwest (Caldbick, 2010).
                             </p>
                             <button onClick={incrementCounter}
                                     className={'m-[1vh] bg-blue-500 hover:bg-blue-700 text-white py-[0.5vh] px-4 rounded-full font-sans text-[2vh] ml-auto transition-colors'}>
@@ -517,9 +540,9 @@ export default function App() {
                             <p className={"h-[50%] overflow-y-scroll"}>
                                 The Klickitat, soon after attempting an escape, found themselves in a standoff near
                                 modern day Battle Ground. Oddly enough, the standoff never actually resulted in a
-                                “battle” as the name would make it seem (Ruth), but rather, ended short when Chief
+                                “battle” as the name would make it seem (Ruth, 2018), but rather, ended short when Chief
                                 Umtuch was shot by an unknown individual, resulting in a quick surrender and return to
-                                the Barracks (Caldbick).
+                                the Barracks (Caldbick, 2010).
                             </p>
                             <button onClick={incrementCounter}
                                     className={'m-[1vh] bg-blue-500 hover:bg-blue-700 text-white py-[0.5vh] px-4 rounded-full font-sans text-[2vh] ml-auto transition-colors'}>
@@ -570,8 +593,8 @@ export default function App() {
                             <h2 className={'text-[4vh] font-bold font-sans'}>A Resort by the Volcano</h2>
                             <p className={""}>
                                 The lake was originally a privately owned resort in the 1900s, starting as a small
-                                swimming area in 1916 (Genis) owned by Henry Blystone. The resort was a popular spot for
-                                swimming and fishing at the time (Genis)
+                                swimming area in 1916 (Genis, 2021) owned by Henry Blystone. The resort was a popular spot for
+                                swimming and fishing at the time (Genis, 2021)
                             </p>
                             <button onClick={incrementCounter}
                                     className={'m-[1vh] bg-blue-500 hover:bg-blue-700 text-white py-[0.5vh] px-4 rounded-full font-sans text-[2vh] ml-auto transition-colors'}>
@@ -623,7 +646,7 @@ export default function App() {
                             <p className={""}>
                                 Some time around the 1960s, it was becoming evident that the state of Washington was
                                 interested in converting the resort into a state park, purchasing surrounding real
-                                estate (Hewitt)...
+                                estate (Hewitt, 2023)...
                             </p>
                             <button onClick={incrementCounter}
                                     className={'m-[1vh] bg-blue-500 hover:bg-blue-700 text-white py-[0.5vh] px-4 rounded-full font-sans text-[2vh] ml-auto transition-colors'}>
@@ -675,7 +698,7 @@ export default function App() {
                             <p className={""}>
                                 In 1968, the state of Washington officially converted the resort into a state park.
                                 Although some amenities have changed, the park still allows for the rental of cabins,
-                                and is currently a popular fishing spot. (Genis)
+                                and is currently a popular fishing spot. (Genis, 2021)
                             </p>
                             <button onClick={incrementCounter}
                                     className={'m-[1vh] bg-blue-500 hover:bg-blue-700 text-white py-[0.5vh] px-4 rounded-full font-sans text-[2vh] ml-auto transition-colors'}>
@@ -719,17 +742,17 @@ export default function App() {
                     <>
                         <div className={'absolute p-[10%] w-[40%] h-full z-10 text-left text-[1.5vh]'}>
                             <span className={'text-[8vh] font-bold font-sans'}>Now</span>
-                            <h1>Geography</h1>
+                            <h1>Geography and Recreation</h1>
                         </div>
                         <div
                             className={'absolute p-[10%] w-[70%] left-[30%] h-full z-10 text-left text-[2.5vh] flex flex-col'}>
                             <h2 className={'text-[4vh] font-bold font-sans'}>Human Intervention</h2>
                             <p className={""}>
                                 Turning a crater lake into a recreational area means decisions must be made around it.
-                                With no rivers connecting the lake to any other water way (Hewitt), fish aren't
+                                With no rivers connecting the lake to any other water way (Hewitt, 2023), fish aren't
                                 available. The solution though, as done when it was a resort and now, is to manually
                                 restock the fish
-                                specifically for recreational purposes (Genis).
+                                specifically for recreational purposes (Genis, 2021).
                             </p>
                             <button onClick={incrementCounter}
                                     className={'m-[1vh] bg-blue-500 hover:bg-blue-700 text-white py-[0.5vh] px-4 rounded-full font-sans text-[2vh] ml-auto transition-colors'}>
@@ -773,7 +796,7 @@ export default function App() {
                     <>
                         <div className={'absolute p-[10%] w-[40%] h-full z-10 text-left text-[1.5vh]'}>
                             <span className={'text-[8vh] font-bold font-sans'}>Now</span>
-                            <h1>Geography</h1>
+                            <h1>Geography and Recreation</h1>
                         </div>
                         <div
                             className={'absolute p-[10%] w-[70%] left-[30%] h-full z-10 text-left text-[2.5vh] flex flex-col'}>
@@ -782,15 +805,63 @@ export default function App() {
                                 Being crater lake also results in another interesting
                                 quirk, which is its depth. The edge of the lake is relatively shallow, having that area
                                 marked for swimming, while it can often get surprisingly deep, more than is usual for
-                                lakes formed by other means (Genis).
+                                lakes formed by other means (Genis, 2021).
                             </p>
                             <button onClick={incrementCounter}
                                     className={'m-[1vh] bg-blue-500 hover:bg-blue-700 text-white py-[0.5vh] px-4 rounded-full font-sans text-[2vh] ml-auto transition-colors'}>
                                 Next
                             </button>
                         </div>
+                        <Canvas
+                            style={{
+                                width: '120vh',
+                                height: '80vh',
+                                margin: "auto",
+                                position: 'absolute',
+                                top: 0,
+                                left: 0,
+                                zIndex: 20,
+                                pointerEvents: "none"
+                            }}
+                            shadows
+                        >
+                            <ambientLight intensity={Math.PI / 2}/>
+                            <directionalLight
+                                position={[5, 5, 5]}
+                                intensity={1}
+                                castShadow
+                                shadow-mapSize-width={1024}
+                                shadow-mapSize-height={1024}
+                                shadow-camera-far={50}
+                                shadow-camera-left={-10}
+                                shadow-camera-right={10}
+                                shadow-camera-top={10}
+                                shadow-camera-bottom={-10}
+                            />
+                            <Suspense fallback={<Loader/>}>
+                                <Page9/>
+                            </Suspense>
+                        </Canvas>
                     </>
                 )
+            case 10:
+                return (<>
+                    <div className={'absolute p-[10%] w-[100%] h-full z-10 text-left text-[1.5vh]'}>
+                        <h1 className={"text-[5vh]"}>Want to Learn more?</h1>
+                        <br/>
+                        <ul className={"text-[3vh] list-disc"}>
+                            <li>
+                                <a href="https://parks.wa.gov/find-parks/state-parks/battle-ground-lake-state-park"><p
+                                    className={"text-blue-500"}>Check out the wa.gov page for travel info!</p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/static/supplementary.pdf"><p
+                                    className={"text-blue-500"}>Supplementary Information (Sources and additional research)</p></a>
+                            </li>
+                        </ul>
+                    </div>
+                </>)
             default:
 
         }
@@ -811,10 +882,11 @@ export default function App() {
                         className={"text-blue"}>← Go back a page
                 </button>
             </div>
-            <div className={"fixed bottom-[1vh] right-[3vh] text-black font-sans text-xl p-5"}>
-                <p>Modelled and developed entirely, with blood, sweat, and tears, by <a href={"https://ineshd.com"}
+            <div className={"fixed bottom-[1vh] right-[3vh] text-black font-sans text-[2vh] p-5"}>
+                <p>View in Fullscreen! Modelled and developed entirely, with blood, sweat, and tears, by <a href={"https://ineshd.com"}
                                                                                         className={"text-blue-500"}>Inesh
-                    Dey</a>.</p>
+                    Dey</a>, using <a href="https://react.dev/" className={"text-blue-500"}>React</a> and <a href="https://threejs.org/" className={"text-blue-500"}>Three.js</a>
+                </p>
             </div>
             <div
                 className={`fixed mb-5 h-[80vh] bg-gray-800 relative rounded-2xl shadow-2xl overflow-clip transition-all`}
